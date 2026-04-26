b2a2bb07fca3ae74d09db3e5d9fe19f03b3a25f8
RAJATSINGH <rajat.singh20242@lpu.in>
2026-04-26 10:13:12 +0530

    update project files

--- rkr-school-reimagined/app.js
+++ rkr-school-reimagined/app.js
@@ -36,6 +36,10 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
