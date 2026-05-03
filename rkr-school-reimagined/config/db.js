8687f1adf9e19f155d1db92544899a18541987bf
RAJATSINGH <rajat.singh20242@lpu.in>
2026-05-03 22:14:49 +0530

    fix: make EJS safer and remove process exit on DB error

--- rkr-school-reimagined/config/db.js
+++ rkr-school-reimagined/config/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err);
-    process.exit(1);
+    // Don't exit process in serverless environment
   }
 };
 
